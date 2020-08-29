29b2acf3bfaffe4edd2e0ec480448559acca4a0c
Zakareya Alatoli <zalatoli.business@gmail.com>
2020-08-29 12:16:41 -0400

    UI and Hitboxes
    -Added rudimentary menus
    -Added hitbox/hurtbox interaction

---
+++ Assets/Scripts/Hitbox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider))]
+public class Hitbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Collider>().isTrigger = true;
+        gameObject.layer = LayerMask.NameToLayer("Hitbox");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Hurtbox>()?.OnHit(this);
+    }
+}
---
+++ Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Hurtbox : MonoBehaviour
+{
+    public void OnHit(Hitbox hitter)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Collider>().isTrigger = false;
+        gameObject.layer = LayerMask.NameToLayer("Hurtbox");
+    }
+}
