37eed9b09ee3d0c704604f6a37aaf74d9850445a
Zakareya Alatoli <zalatoli.business@gmail.com>
2020-08-30 12:41:41 -0400

    Gameplay HUD, Timer
    -Added countdown match timer
    -Added main gameplay UI

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pratfall
+{
+    [System.Serializable]
+    public class Timer
+    {
+        public float currentTime;
+        public float maxTime;
+        public event System.Action TimeUp;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        public void Tick()
+        {
+            if (currentTime > 0.0f)
+            {
+                currentTime -= Time.deltaTime;
+                if (currentTime <= 0.0f)
+                    TimeUp?.Invoke();
+            }
+        }
+    }
+
+    public class Clock : MonoBehaviour
+    {
+        public Timer time;
+        public Text display;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            time.Tick();
+            System.TimeSpan span = System.TimeSpan.FromSeconds(time.currentTime);
+            display.text = span.ToString("hh':'mm':'ss");
+        }
+    }
+}
--- Assets/Scripts/ObjectTracker.cs
+++ Assets/Scripts/ObjectTracker.cs
@@ -2,49 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectTracker : MonoBehaviour
+namespace Pratfall
 {
-    public Transform[] tracked;
-    public Vector3 offset;
-    // Start is called before the first frame update
-    void Start()
+    public class ObjectTracker : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        float minX, minY, maxX, maxY;
-        minX = minY = maxX = maxY = 0.0f;
-
-        if(tracked.Length != 0)
+        public Transform[] tracked;
+        public Vector3 offset;
+        // Start is called before the first frame update
+        void Start()
         {
-            minX = maxX = tracked[0].position.x;
-            minY = maxY = tracked[0].position.y;
+
         }
-        if(tracked.Length > 1)
+
+        // Update is called once per frame
+        void LateUpdate()
         {
-            minX = maxX = tracked[0].position.x;
-            minY = maxY = tracked[0].position.y;
-            for (int i = 1; i < tracked.Length; i++)
+            float minX, minY, maxX, maxY;
+            minX = minY = maxX = maxY = 0.0f;
+
+            if (tracked.Length != 0)
             {
-                Vector3 pos = tracked[i].position;
-                if (pos.x < minX) minX = pos.x;
-                if (pos.x > maxX) maxX = pos.x;
-                if (pos.y < minY) minY = pos.y;
-                if (pos.y > maxY) maxY = pos.y;
+                minX = maxX = tracked[0].position.x;
+                minY = maxY = tracked[0].position.y;
             }
+            if (tracked.Length > 1)
+            {
+                minX = maxX = tracked[0].position.x;
+                minY = maxY = tracked[0].position.y;
+                for (int i = 1; i < tracked.Length; i++)
+                {
+                    Vector3 pos = tracked[i].position;
+                    if (pos.x < minX) minX = pos.x;
+                    if (pos.x > maxX) maxX = pos.x;
+                    if (pos.y < minY) minY = pos.y;
+                    if (pos.y > maxY) maxY = pos.y;
+                }
+            }
+
+            Vector3 finalPos = new Vector3();
+            finalPos.x = (maxX + minX) / 2.0f;
+            finalPos.y = (maxY + minY) / 2.0f;
+            if (Mathf.Abs(maxX - minX) > Mathf.Abs(maxY - minY))
+                finalPos.z = Mathf.Abs(maxX - minX) * -1;
+            else
+                finalPos.z = Mathf.Abs(maxY - minY) * -1;
+
+            transform.position = finalPos + offset;
         }
-
-        Vector3 finalPos = new Vector3();
-        finalPos.x = (maxX + minX) / 2.0f;
-        finalPos.y = (maxY + minY) / 2.0f;
-        if (Mathf.Abs(maxX - minX) > Mathf.Abs(maxY - minY))
-            finalPos.z = Mathf.Abs(maxX - minX) * -1;
-        else
-            finalPos.z = Mathf.Abs(maxY - minY) * -1;
-
-        transform.position = finalPos + offset;
     }
 }
