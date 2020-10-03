cd3d969a5d41df2878f3fab681803dd28e4448fd
Zakareya Alatoli <zalatoli.business@gmail.com>
2020-10-03 18:06:45 -0400

    Round Logic
    -WIP logic for spawning in characters and giving players control of them.

--- Assets/Scripts/Input/PlayerSetup.cs
+++ Assets/Scripts/Input/PlayerSetup.cs
@@ -11,18 +11,29 @@ namespace Pratfall.Input
     [RequireComponent(typeof(PlayerInputManager))]
     public class PlayerSetup : MonoBehaviour
     {
+        public static List<PlayerInput> players { get; private set; }       
         PlayerInputManager inputManager;
         
 
         void OnPlayerJoined(PlayerInput player)
         {
             player.name = "Player" + inputManager.playerCount.ToString();
+            players.Add(player);
             player.transform.SetParent(transform);
+            Debug.Log(player.name + " joined");
         }
+
+        void OnPlayerLeft(PlayerInput player)
+        {
+            players.Remove(player);
+            Debug.Log(player.name + " left");
+        }
+
         // Start is called before the first frame update
         void Start()
         {
             inputManager = GetComponent<PlayerInputManager>();
+            players = new List<PlayerInput>();
         }
     }
 }
---
+++ Assets/Scripts/RoundLogic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pratfall.Input;
+using Pratfall.Characters;
+
+namespace Pratfall {
+    public class RoundLogic : MonoBehaviour
+    {
+        public IControllable[] characters;
+        public void RoundStart()
+        {
+            for (int i = 0; i < PlayerSetup.players.Count; i++)
+            {
+                PlayerSetup.players[i].GetComponent<InputHandler>().controllables =
+                new IControllable[] { FindObjectOfType<Character>() as IControllable };
+
+                AssignPlayerToCharacter(PlayerSetup.players[i].GetComponent<InputHandler>(), characters[i]);
+            }  
+        }
+
+        public void RoundEnd()
+        {
+            //
+        }
+
+        void AssignPlayerToCharacter(InputHandler player, IControllable character)
+        {
+            //
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            RoundStart();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
