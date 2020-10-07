64dbc1cc048362fcb793bf5a52f6048660b925ee
Zakareya Alatoli <zalatoli.business@gmail.com>
2020-10-06 20:17:58 -0400

    Cursor Logic Complete
    -Added the ability to select UI elements from individual cursors

--- Assets/Cursor.cs
+++ Assets/Cursor.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Pratfall.Input
 {
+    /// <summary>
+    /// Floating player-specific cursor
+    /// </summary>
     public class Cursor : MonoBehaviour, IControllable
     {
-        public float speed = 5f;
+        GraphicRaycaster m_Raycaster;
+        PointerEventData m_PointerEventData;
+        EventSystem m_EventSystem;
+
+        public float speed = 7.5f;
         public void OnAttack(Vector2 direction)
         {
             
@@ -23,17 +32,48 @@ namespace Pratfall.Input
         }
 
         public void OnMove(Vector2 direction)
-        {
+        {        
             transform.Translate(direction * speed);
-            //TO DO: Prevent going off screen
+        }
+
+        void Update()
+        {
+            Vector3 pos = transform.position;
+            pos.x = Mathf.Clamp(pos.x, 0f, Screen.width);
+            pos.y = Mathf.Clamp(pos.y, 0f, Screen.height);
+            transform.position = pos;
         }
 
         void Select()
         {
-            //TO DO: Raycast from the cursor position to select objects on screen
+            //TAKEN FROM UNITY DOCS
+            //Set up the new Pointer Event
+            m_PointerEventData = new PointerEventData(m_EventSystem);
+            //Set the Pointer Event Position to that of the mouse position
+            m_PointerEventData.position = transform.position;
+
+            //Create a list of Raycast Results
+            List<RaycastResult> results = new List<RaycastResult>();
+
+            //Raycast using the Graphics Raycaster and mouse click position
+            m_Raycaster.Raycast(m_PointerEventData, results);
+
+            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            foreach (RaycastResult result in results)
+            {
+                IPointerClickHandler selectedObject = result.gameObject.GetComponent<IPointerClickHandler>();
+                if (selectedObject != null)
+                    selectedObject.OnPointerClick(m_PointerEventData);
+            }
         }
 
         // Start is called before the first frame update
-        void Start() { }
+        void Start()
+        {
+            //Fetch the Raycaster from the GameObject (the Canvas)
+            m_Raycaster = FindObjectOfType<Canvas>().GetComponent<GraphicRaycaster>();
+            //Fetch the Event System from the Scene
+            m_EventSystem = EventSystem.current;
+        }
     }
 }
---
+++ Assets/Scripts/Debug/DebugLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pratfall.Debugging
+{
+    public class DebugLogger : MonoBehaviour
+    {
+        public void Print(string message)
+        {
+            Debug.Log(message);
+        }
+        // Start is called before the first frame update
+        void Start() { }
+    }
+}
